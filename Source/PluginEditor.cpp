f398033b94ec5bd5ba02232f41279b2c08a45713
LostNoris <sellers149@gmail.com>
2023-01-28 16:08:12 +0000

    gui

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -26,11 +26,31 @@ SimpleDelayAudioProcessorEditor::~SimpleDelayAudioProcessorEditor()
 void SimpleDelayAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    //g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.fillAll(juce::Colours::black);
+
+    g.setColour(juce::Colours::darkseagreen);
+    juce::Rectangle<float> backgroundRect = getLocalBounds().toFloat();
+    int numBackgroundRects = 60;
+    juce::Point<float> offset = backgroundRect.getBottomRight() / numBackgroundRects;
+
+    for (int i = 0; i < numBackgroundRects; ++i)
+    {
+        g.drawRect(backgroundRect);
+        backgroundRect += offset;
+    }
+
+    g.setColour(juce::Colours::cornflowerblue);
+    g.fillRect(20, 20, 100, 100);
+
+    g.setColour(juce::Colours::darkred);
+    g.fillRect(120, 20, 100, 100);
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawText(" This Square Is Blue ", juce::Rectangle(20, 20, 300, 100), juce::Justification::verticallyCentred, 1); 
+    //g.drawFittedText ("Hallo Wurld", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void SimpleDelayAudioProcessorEditor::resized()
